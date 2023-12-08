e98dc3a99af8b690d1c8696c009e2be635a7fa1a
Jack Edge <js77js@yahoo.co.uk>
2023-12-08 00:19:20 +0000

    better comments

--- perl-compile-link-and-run-testCpp.pl
+++ perl-compile-link-and-run-testCpp.pl
@@ -1,16 +1,16 @@
-#!/usr/bin/perl
 use warnings;
 use strict;
-use feature qw(say);
+use feature qw /say/;
 use Cwd;
 
 # compile to get .o object files
 `clang++ -c test.cpp test-joe.cpp`;
-# link files and produce .exe
+# link files to produce .exe
 `clang++ test.o test-joe.o -o test-exe-file.exe`;
-# run (using full path name of the executable)
+# get the full path name of the executable
 my $cwd = getcwd;
 my $exe = $cwd."/test-exe-file.exe";
-# print program output to screen
+# run exe. store program output in variable (hidden by default when run from perl file) 
 my $output = `$exe`;
+# print exe output to screen
 say $output;
